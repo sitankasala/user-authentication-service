05714e0a1addf188c6d2579d5d29a3fb91369919
Sid <tankasala.siddhartha@gmail.com>
2023-05-18 07:48:57 +0530

    first commit

---
+++ src/test/java/com/ecom/clothing/userauthentication/UserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.clothing.userauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
